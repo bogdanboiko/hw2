ba8592c89d5b5051be95d50fb93692972a69e87a
Bogdan Boiko <bogdanboiko19996@gmail.com>
2021-11-14 15:52:15 +0300

    Create menu fragment file with binds

---
+++ app/src/main/java/com/example/hw2/fragment/MenuFragment.kt
@@ -0,0 +1,24 @@
+package com.example.hw2.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.hw2.R
+import com.example.hw2.databinding.MenuFragmentBinding
+
+class MenuFragment : Fragment(R.layout.menu_fragment) {
+    private var _binding : MenuFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        _binding = MenuFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
