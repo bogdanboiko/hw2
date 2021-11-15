62ad7629b49fece784fa1486999e7809a2822166
Bogdan Boiko <bogdanboiko19996@gmail.com>
2021-11-15 17:11:59 +0300

    Fix main activity

--- app/src/main/java/com/example/hw2/MainActivity.kt
+++ app/src/main/java/com/example/hw2/MainActivity.kt
@@ -1,30 +1,22 @@
 package com.example.hw2
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.findFragment
 import com.example.hw2.databinding.ActivityMainBinding
 import com.example.hw2.fragment.*
 
-class MainActivity : AppCompatActivity(R.layout.activity_main), ItemClickListener {
+class MainActivity : AppCompatActivity(), ItemClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        supportFragmentManager.beginTransaction().apply {
-            replace(binding.itemsContainer.id, MenuFragment())
-            replace(binding.listContainer.id, ListFragment(), "tag_list")
-            commit()
-        }
     }
 
-    override fun onItemClicked(item: String) {
+    override fun onItemClicked(id: Int) {
         (supportFragmentManager.findFragmentByTag(
             "tag_list"
-        ) as? ListFragment)?.onItemPressed(item)
+        ) as? ListFragment)?.onItemPressed(id)
     }
 }
